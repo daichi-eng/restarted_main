7e594e5dabcf2cade8ff19180bae10754c9cd973
daichi yamano <daichi.free.life@gmail.com>
2021-05-11 08:13:36 +0900

    自己紹介欄（aside、footer）の追記とレイアウト修正

--- resources/views/layouts/user/aside.blade.php
+++ resources/views/layouts/user/aside.blade.php
@@ -1,5 +1,5 @@
 
-	<div class="card">
+	<div class="card mb-3">
 		<div class="card-header">ユーザ情報</div>
 
 		<!-- card-body Start -->
@@ -23,3 +23,49 @@
 		</div><!-- card-body End -->
 			
 	</div><!-- card End -->
+
+
+	<div class="card mb-3">
+		<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Developer Image</text></svg>
+		
+		<div class="card-header">開発者の自己紹介</div>
+
+		<div class="card-body">
+
+			<p class="card-text">
+				はじめまして！エンジニアのだいちです。
+			</p>
+			<p class="card-text">
+				趣味と勉強を兼ねてツールを開発しています。
+				ソースコードはGithubに公開しているので批評やアドバイスいただけると嬉しいです(^^)/
+			</p>
+			<p class="card-text">
+				<strong>ツールは無料でご利用いただけます。</strong>
+			</p>
+
+			<a class="btn btn-success m-2" href="https://twitter.com/Daichi_Started"><i class="ml-1 fab fa-twitter mr-1"></i>Twitter</a>
+			
+			<a class="btn btn-success m-2" href="https://github.com/daichi-eng"><i class="fab fa-github mr-1"></i>Github</a>
+
+			<a class="btn btn-success m-2" href="https://restarted.site/"><i class="fas fa-blog mr-1"></i>ブログ</a>
+
+		</div>
+	</div>
+
+	<div class="card mb-3">
+		<div class="card-header">お問い合わせはこちら</div>
+
+		<!-- card-body Start -->
+		<div class="card-body container">
+			
+			<p class="card-text">
+				エラーを見つけたときに<a href="https://restarted.site/contact_200906/">問い合わせページ</a>からご連絡いただけるととても喜びます。
+			</p>
+			
+			<a class="btn btn-success m-2 " href="https://restarted.site/"></i>問い合わせ</a>
+
+		</div><!-- card-body End -->
+			
+	</div><!-- card End -->
+	
+</div>
--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -1,19 +1,20 @@
 
 {{-- Start footer --}}
-<footer class="footer bg-info py-3">
+<div class="footer bg-info py-3">
 	<div class="container">
 		
 		<div class="row mb-2">
 			<div class="col-md-6 col-sm-12 mb-2">
 				
-				<div>開発者のアカウント</div>
+				<a class="btn text-light" href="https://twitter.com/Daichi_Started">Twitter<i class="ml-1 fab fa-twitter"></i></a>
 				
-				Twitter<a class="text-light m-2" href="https://twitter.com/Daichi_Started"><i class="ml-1 fab fa-twitter"></i></a>
+				<a class="btn text-light" href="https://github.com/daichi-eng">Github<i class="fab fa-github"></i></a>
+			
+				<a class="btn text-light" href="https://restarted.site/"><i class="fas fa-blog"></i>ブログ</a>
 				
-				Github<a class="text-light m-2" href="https://github.com/daichi-eng"><i class="fab fa-github"></i></a>
 			</div>
 			
-			<div class="col-md-4 col-sm-12 mb-2">
+			<div class="col-md-6 col-sm-12 mb-2">
 				<a class="btn text-light" href="{{ route('admin.home') }}">
 					システム管理者ページ
 				</a>
@@ -25,6 +26,6 @@
 		</div>
 		
 	</div>
-</footer>
+</div>
 
 {{-- End footer --}}
